396297c9e1ddf6ac8f91a41dd6eb665d477267c7
SalvLav <richardjthies95@gmail.com>
2023-09-06 01:26:59 -0500

    Added instructions function

--- camel_case_revamp.py
+++ camel_case_revamp.py
@@ -1,12 +1,13 @@
 def main():	
 	display_banner()
+	instructions()
 	sentence = input('Enter your sentence: ')
 	output = camelcase(sentence)
 	print(output)
 
 def display_banner():
-	""" Display program name in banner """
-	msg = 'AWSOME camelCaseGenerator PROGRAM'
+	""" Display program name in banner """# can use docstrings anywhere, not just at the beginning
+	msg = 'AWESOME camelCaseGenerator PROGRAM'
 	stars = '*' * len(msg)
 	print(f'\n {stars} \n {msg} \n {stars}\n') 
 
@@ -18,6 +19,9 @@ def camelcase(sentence):
 	# Slices don't produce index out of bounds errors. 
 	# So this still works on empty strings, strings of length 1
 	return upper_camel_cased[0:1].lower() + upper_camel_cased[1:] 
+
+def instructions():
+	print('Enter a message and the program will convert it to camelcase.')
 
 if __name__ == '__main__':
 	main()
